7eec2c2211fcab7d22350a258f0693af9e4d537b
ggcr <ggcristian@icloud.com>
2021-06-20 01:56:51 +0200

    Landing page html and css implemented

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  purge: [
+     './resources/**/*.blade.php',
+     './resources/**/*.js',
+     './resources/**/*.vue',
+   ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {
+          fontFamily: {
+              poppins: ['Poppins', 'Open-Sans'],
+              formula: ['Formula1'],
+          },
+          backgroundImage: theme => ({
+              'pink-pattern': "url('/img/FrozenDreams.png')",
+              'Bottom-pattern': "url('/img/Bottom.png')",
+              'deepblue-pattern': "url('/img/deepblue.png')",
+              'morpheus-pattern': "url('/img/morpheus.png')",
+              'near-pattern': "url('/img/near.png')",
+              'rare-pattern': "url('/img/rare.png')",
+              'saint-pattern': "url('/img/saint.png')",
+              'wild-pattern': "url('/img/wild.png')",
+
+          }),
+          colors: {
+              titlePink: '#500C52',
+          }
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+}
